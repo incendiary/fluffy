a3239af7f970d611f608cfe49693c04eb43ccd74
Adam Horsewood <adam.horsewood@mwrinfosecurity.com>
2017-09-25 15:25:29 +0100

    now pulling azure info

---
+++ Classes/account.py
@@ -0,0 +1,168 @@
+import json
+import random
+import os
+import base64
+
+class Api_access_key(object):
+
+    def __init__(self, keydescriptor, createdate, user):
+        self.api_access_key = keydescriptor
+        self.api_access_key_create_date = createdate
+        self.user = user
+
+        self.api_access_key_unused = False
+        self.api_access_key_last_used_too_long = False
+        self.api_key_too_old = False
+
+
+    def set_create_date(self, createdate, tooold):
+        self.api_access_key_create_date = createdate
+        self.api_key_too_old = tooold
+
+    def set_last_used(self, access_key_last_used_date, api_access_key_last_used_too_long):
+        self.api_access_key_last_used = access_key_last_used_date
+        self.api_access_key_last_used_too_long = api_access_key_last_used_too_long
+
+
+    def set_unused(self):
+        #logic is, if the access key is unused, then the last time it was used is to long by default.
+        self.api_access_key_last_used_too_long = True
+        self.api_access_key_unused = True
+
+    def createselfdict(self):
+        self.dict = {'user': self.user,
+                     'api_access_key': self.api_access_key,
+                     'api_access_key_create_date': str(self.api_access_key_create_date),
+                     'api_access_key_unused': self.api_access_key_unused,
+                     'api_access_key_last_used_too_long': self.api_access_key_last_used_too_long,
+                     'api_key_too_old': self.api_key_too_old
+                     }
+
+    def returndictkey(self):
+        self.createselfdict()
+        return self.dict
+
+    def returnjsonkey(self):
+        self.createselfdict()
+        return json.dumps(self.dict)
+
+
+
+
+class Account(object):
+    def __init__(self, username, avatar=False):
+        self.username = username
+        self.api_access_keys_list = []
+        self.inline_enabled = False
+        self.mfa_enabled = False
+        self.administrative = False
+        self.group_membership_enabled = False
+        self.administrative_not_mfa = False
+        self.groups = []
+        self.last_used = None
+        self.create_date = None
+        self.service_account = None
+
+        if not avatar:
+            #no avatar passed, setting random one
+            avatar_directory = "imgs/avatars/"
+            with open(avatar_directory + random.choice(os.listdir(avatar_directory)), "rb") as image_file:
+                data = image_file.read()
+                self.avatar = data.encode("base64").replace('\n', '')
+
+    def set_last_used(self, date):
+        #used to store the last time an account was used, seperate to key usage if supproted.
+        self.last_used = date
+
+    def return_last_used(self):
+        return self.last_used
+
+    def set_create_date(self, createdate):
+        self.create_date = createdate
+
+    def return_create_date(self):
+        return self.create_date
+
+    def set_user_id(self, id):
+        self.user_id = id
+
+    def return_user_id(self):
+        return self.user_id
+
+    def appended_api_access_key(self, key):
+        #append api access key objects to the api access key list
+        self.api_access_keys_list.append(key)
+
+    def return_api_access_keys_list(self):
+        #returns api acces key lists
+        return self.api_access_keys_list
+
+    def return_api_access_keys_json(self):
+        #attempts to return keys as json
+        list = []
+        for key in self.api_access_keys_list:
+            list.append(key.returnjsonkey())
+        return list
+
+    def set_service_account(self, state):
+        self.service_account = state
+
+    def return_service_account(self):
+        return self.service_account
+
+    def set_mfa_enabled(self, state):
+        self.mfa_enabled = state
+
+    def user_can_login(self, login):
+        #used to store if a user can login or if its just api access.
+        self.can_login =  login
+
+    def do_checks(self):
+        #tempory list of checks
+        # password and key aging
+        # 2fa on critical accounts
+        pass
+
+
+
+class Aws_account(Account):
+
+    def set_inline_enabled(self, state):
+        #Maybe aws specific
+        self.inline_enabled = state
+        self.inline_policy_list = []
+
+    def append_inline_policy(self, policy):
+        self.inline_policy_list.append(policy)
+
+    def append_group(self, group):
+        self.groups.append(group)
+
+    def set_arn(self, arn):
+        self.arn = arn
+
+    def return_arn(self):
+        return self.arn
+
+
+
+    def do_aws_checks(self, known_aws_admin_policies, known_aws_admin_groups):
+        # known_aws_admin_policies & known_aws_admin_groups both come via the config file.
+
+        # Inline Policy Check
+        if self.inline_enabled:
+            for policy in self.inline_policy_list:
+                if policy['PolicyName'] in known_aws_admin_policies:
+                    self.administrative = True
+
+        # Group membership Check
+        if self.group_membership_enabled:
+            for group in self.groups:
+                if group['GroupName'] in ['Administrators', 'Admin']:
+                    self.administrative = True
+                if group['GroupName'] in known_aws_admin_groups:
+                    self.administrative = True
+
+        if self.administrative:
+            if not self.mfa_enabled:
+                self.administrative_not_mfa = True
---
+++ Common/common.py
@@ -0,0 +1,113 @@
+__author__ = 'adz'
+import argparse
+import logging
+import ConfigParser
+import datetime
+import os
+from os import environ
+from beeprint import pp
+
+def createdirifnotexists(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+def yesnoquestion(string):
+    while True:
+        response = raw_input("%s [Y/n]" % (string)).lower()
+        while response not in ("yes", "no", "y", "n"):
+            response = raw_input("%s [Y/n]" % (string)).lower()
+        if response == "no" or response == "n":
+            return False
+            break
+        if response == "yes" or response == "y":
+            return True
+            break
+
+def get_list_from_config_parser(option, sep=',', chars=None):
+    """Return a list from a ConfigParser option. By default,
+       split on a comma and strip whitespaces."""
+    return [ chunk.strip(chars) for chunk in option.split(sep) ]
+
+
+def loggingobject(args):
+    #creates the logging object
+    createdirifnotexists(args.log)
+    filename = "%s/%s" % (args.log, args.logname)
+
+    logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M',
+                    filename=filename,
+                    filemode='w')
+
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+    console.setFormatter(formatter)
+    logging.getLogger('').addHandler(console)
+    return logging
+
+
+
+def config_parser_rename_section(config, section_from, section_to, destructive=False):
+    if destructive:
+        config.remove_section(section_to)
+    items = config.items(section_from)
+    config.add_section(section_to)
+    for item in items:
+        config.set(section_to, item[0], item[1])
+    config.remove_section(section_from)
+
+def initalsetup():
+
+    parser = argparse.ArgumentParser(description='This is fluffy, I try to automate Cloud auditing elemetns'
+                                                 'so you can get on with more interesting things')
+
+
+
+    parser.add_argument('--cfg', '-c', help="Where is the config file?", required=True)
+
+
+    parser.add_argument('--log', '-l', help="any log dir? defaults to logs",
+                        required=False, default='logs')
+
+    parser.add_argument('--logname', '-lf', help="log file name defaults too: %s.<date>.log", default="%s.log"
+                        % (datetime.datetime.now().strftime("%Y-%m-%d_%H_%M_%S")))
+
+    args = parser.parse_args()
+    #logging to file and screen
+
+
+    config = ConfigParser.SafeConfigParser()
+
+    #check for pre existing aws creds
+    awsconfig = os.path.expanduser('~') + '/.aws/credentials'
+    if os.path.isfile(awsconfig) and os.access(awsconfig, os.R_OK):
+        awsresponse = yesnoquestion("[A] AWS credential folder found, would you like me to look here for credentials?")
+
+
+    #should only be true, if above check as returned so.
+    if awsresponse:
+        config.read([os.path.expanduser(args.cfg)])
+        config.read(['/Users/adz/.aws/credentials'])
+        config_parser_rename_section(config, 'default', 'awscredentials', destructive=True)
+
+    else:
+        config.read([os.path.expanduser(args.cfg)])
+
+
+    if config.get('azure', 'azureauthlocation') in os.environ:
+        azureresponse = yesnoquestion(
+            "[A] Azure credential file in envvar (%s) found, would you like me to look here for credentials?"
+            % environ.get(config.get('azure', 'azureauthlocation')))
+    else:
+        ### logic here - no env var set TBD
+        azureresponse = None
+
+    if azureresponse:
+        config.set('azure', 'azureauthenvvar', 'Found')
+
+    logging = loggingobject(args)
+
+    return logging, args, config
+
